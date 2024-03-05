5b17f34b0a320eb8b4ac22838b8a7e851d9081ee
rapaparara <rafiblue6@gmail.com>
2024-03-05 23:48:31 +0800

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Judul</h6>
+            </div>
+            <div class="card-body">
+                <h4>Hello</h4>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
